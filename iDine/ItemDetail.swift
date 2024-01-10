4b2fd93ed5ff5c6e93a93dc26bc9552f2c0d70de
Bill-Yuyi <a274683718@gmail.com>
2024-01-09 19:56:48 -0800

    add menu list

---
+++ iDine/ItemDetail.swift
@@ -0,0 +1,40 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Yuyi Wang on 1/9/24.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    let item: MenuItem
+    var body: some View {
+        VStack{
+            ZStack(alignment:.bottomTrailing){
+                Image(item.mainImage)
+                    .resizable()
+                    .scaledToFit()
+                Text("Photo: \(item.photoCredit)")
+                    .padding(4)
+                    .background(.black)
+                    .font(.caption)
+                    .foregroundColor(.white)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(item.description)
+                .padding()
+            Spacer()
+        }
+        .navigationTitle(item.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    NavigationStack{
+        ItemDetail(item: MenuItem.example)
+    }
+    
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Yuyi Wang on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
